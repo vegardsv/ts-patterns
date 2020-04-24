6a62b1967b45dfb2c18224dadf4f67f1d6b4a9c1
Vegard Svendsen <vegardsvendsen@Vegards-MacBook-Pro-3.local>
2020-04-24 13:33:49 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type RemoteData<E, D> =
+  | { type: "NOT_ASKED" }
+  | { type: "LOADING" }
+  | { type: "FAILURE"; error: E }
+  | { type: "SUCCESS"; data: D };
+
+interface Post {
+  id: string;
+  title: string;
+  body: string;
+}
+
+async function fetchPosts(): Promise<RemoteData<Error, Post[]>> {
+  try {
+    const response = await fetch("https://jsojnplaceholder.typicode.com/posts");
+    if (!response.ok) throw await response.json();
+    const data = await response.json();
+    return { type: "SUCCESS", data: data };
+  } catch (e) {
+    return { type: "FAILURE", error: e };
+  }
+}
+
+function App(): JSX.Element {
+  const [posts, setPosts] = useState<RemoteData<Error, Post[]>>({
+    type: "NOT_ASKED",
+  });
+
+  const getPosts = () => {
+    setPosts({ type: "LOADING" });
+    fetchPosts().then((remoteData) => setPosts(remoteData));
+  };
+  switch (posts.type) {
+    case "NOT_ASKED":
+      return (
+        <div style={{ textAlign: "center" }}>
+          <div>Not asked for posts yet</div>
+          <button onClick={getPosts}>Fetch Posts</button>
+        </div>
+      );
+    case "LOADING":
+      return <div>Loading</div>;
+    case "FAILURE":
+      return <div>Something went wrong 😨</div>;
+    case "SUCCESS":
+      return (
+        <div>
+          {posts.data.map((post) => (
+            <article
+              key={post.id}
+              style={{
+                border: "1px solid darkgray",
+                margin: "1rem",
+                padding: "1rem",
+              }}
+            >
+              <h2>{post.title}</h2>
+              <div dangerouslySetInnerHTML={{ __html: post.body }}></div>
+            </article>
+          ))}
+        </div>
+      );
+  }
 }
 
 export default App;
